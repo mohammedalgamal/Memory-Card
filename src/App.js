4f2482254e825435752b150bd89e3c250ed4b6fd
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-05-26 17:45:01 +0300

    Add Footer component and all its Styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import "./Styles/App.css";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,1 +1,14 @@
+import React from 'react';
 import "../Styles/Footer.css";
+import logo from "../Images/Github-logo.png";
+
+export default function Footer() {
+    return (
+      <div className="Footer">
+        <div className="text">Copyright © 2023 Mohammed Algamal</div>
+        <a href="https://github.com/mohammedalgamal/Memory-Card">
+          <img alt="GitHub Logo" src={logo}></img>
+        </a>
+      </div>
+    );
+  }
