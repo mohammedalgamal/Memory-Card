eeae3317ac475632a0ee9a5a1df3ef5d1bdb08e3
mohammedalgamal <mohammedalgamal95@gmail.com>
2023-05-26 22:37:39 +0300

    Create Main Component and add flags Images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Main from "./Components/Main";
 import "./Styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
       <Footer/>
     </div>
   );
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,1 +1,10 @@
+import React from 'react';
 import "../Styles/Main.css";
+
+export default function Main() {
+    return (
+        <div className="Main">
+            Hi
+        </div>
+    )
+}
